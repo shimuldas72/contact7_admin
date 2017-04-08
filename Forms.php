a07dfefd2eaefa1c9f8ccbaf80032d243be21d4a
Shimul <shimuldas72@gmail.com>
2017-04-08 19:56:20 +0600

    first release

---
+++ Forms.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace shimuldas72\forms;
+
+class Forms extends \yii\base\Module
+{
+   public $controllerNamespace = 'shimuldas72\forms\controllers';
+
+   public function init()
+   {
+       parent::init();
+
+       // custom initialization code goes here
+   }
+}
